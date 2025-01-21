7d0a0df19ce19e1f1537ed2a9e38107e113bcc0c
Amber Suman <beingambersuman@gmail.com>
2025-01-22 02:05:43 +0530

    home page animation

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import { TypeAnimation } from 'react-type-animation';
+
 function Home() {
     return (
-        <div className="text-center bg-black h-[83vh] pt-56" style={{ backgroundImage: `url('https://i.imgur.com/5sjYjnf.jpeg')`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover', 'backgroundPosition': 'center' }}>
-            <p className="text-white text-4xl font-bold mb-6">Place and Track</p>
-            <p className="text-white text-4xl font-bold mb-6">all your Orders</p>
-            <p className="text-white text-4xl font-bold mb-6">Here...</p>
+        <div className="text-center bg-black h-[83vh] pt-12" style={{ backgroundImage: `url('https://i.imgur.com/5sjYjnf.jpeg')`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover', 'backgroundPosition': 'center', 'opacity': '0.8' }}>
+            <p className=" text-[#FFC000] text-4xl font-bold mb-32 text-center">InfraTrack</p>
+            <TypeAnimation
+                sequence={[
+                    // Same substring at the start will only be typed once, initially
+                    'Place order of required items',
+                    1000,
+                    'Track your order status',
+                    1000
+                ]}
+                speed={25}
+                style={{ fontSize: '2em' }}
+                repeat={Infinity}
+                className='text-white text-2xl font-bold'
+            />
         </div>
     )
 }
