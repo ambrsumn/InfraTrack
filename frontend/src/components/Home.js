b21cf191e8c25587118f4cefff25a825caa5665e
Amber Suman <beingambersuman@gmail.com>
2025-01-21 17:07:53 +0530

    Update Home.js

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,7 +1,7 @@
 import React from 'react'
 function Home() {
     return (
-        <div className="text-center bg-black h-[83vh] pt-56" style={{ backgroundImage: `url('https://i.imgur.com/e3op7JB.jpeg')`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}>
+        <div className="text-center bg-black h-[83vh] pt-56" style={{ backgroundImage: `url('https://i.imgur.com/5sjYjnf.jpeg')`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover', 'backgroundPosition': 'center' }}>
             <p className="text-white text-4xl font-bold mb-6">Place and Track</p>
             <p className="text-white text-4xl font-bold mb-6">all your Orders</p>
             <p className="text-white text-4xl font-bold mb-6">Here...</p>
